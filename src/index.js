b126751d48807129dc467fb5fff13887994c6697
VasylLymych <vasyllymych20001002@gmail.com>
2021-07-12 12:43:18 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux'
+
+ReactDOM.render(
+    <Provider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
